ce5da5303ac5839dffcbdec19893467cab1437ee
Aishwarya-Lad <aishwarya.lad@harness.io>
2023-11-17 00:48:03 -0800

    just for ti 1

--- test_api_1.py
+++ test_api_1.py
@@ -7,7 +7,7 @@ def test_index_route_1():
     response = app.test_client().get('/')
 
     assert response.status_code == 200
-    assert response.data.decode('utf-8') == 'Testing, Flask!'
+    assert response.data.decode('utf-8') == 'Testing, Flask ....!'
 
 @pytest.mark.get_request
 def test_get_all_books_1():
